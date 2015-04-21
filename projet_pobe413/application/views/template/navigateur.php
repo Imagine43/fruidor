7c12c701733bd3ff234bb132d774c67d05b380f4
Imagine <thomas-migerel@live.FR>
2015-04-22 01:15:55 +0200

    Modifications mineures.
    -Téléchargement et ajout de nouvelles  images.
    -Correction de diverses erreurs et ajouts de liens relatifs.
    -Améliorations mineures de la page Accueil. (Le caroussel fonctionne
    désormais correctement).

--- projet_pobe413/application/views/template/navigateur.php
+++ projet_pobe413/application/views/template/navigateur.php
@@ -14,6 +14,8 @@
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
+    <!-- Coté gauche de la barre de navigation -->
+
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li class="active"><a href="accueil">Description <span class="sr-only"></span></a></li>
@@ -25,11 +27,14 @@
             <li><a href="implantations">Nos Implentations</a></li>
             <li><a href="#">Documentation</a></li>
             <li class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
           </ul>
-        <li><a href="#">Contact</a></li>
+        <li><a href="contact">Contact</a></li>
         </li>
       </ul>
+
+      <!-- Coté droit de la barre de navigation -->
+
+
       <ul class="nav navbar-nav navbar-right">
         <li><a href="#">Link</a></li>
         <li class="dropdown">
@@ -37,6 +42,7 @@
           <ul class="dropdown-menu span2" role="menu">
             <li><a href="profil">Profil</a></li>
             <li><a href="login">Login</a></li>
+            <li><a href="administration">Administration</a></li>
             <li class="divider"></li>
             <li><a href="#">Déconnexion</a></li>
           </ul>
