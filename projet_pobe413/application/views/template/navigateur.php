2f99e978fa642e8ff416edc3eaefaf32828d80cd
Imagine <thomas-migerel@live.FR>
2015-04-22 16:13:56 +0200

    dernier commit

--- projet_pobe413/application/views/template/navigateur.php
+++ projet_pobe413/application/views/template/navigateur.php
@@ -19,14 +19,12 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
         <li class="active"><a href="accueil">Descriptions <span class="sr-only"></span></a></li>
-        <li><a href="nosactivites">Qui sommes-nous ? </a></li>
+        <li><a href="description">Qui sommes-nous ? </a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-toggle="dropdown" data-delay="0" data-close-others="false" role="button" aria-expanded="false">Nos activités <span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
             <li><a href="qualite">Qualités</a></li>
-            <li><a href="implantations">Nos Implentations</a></li>
-            <li class="divider"></li>
-            <li><a href="documentation">Documentations</a></li>
+            <li><a href="implantations">Nos Implantations</a></li>
           </ul>
         <li><a href="contact">Contact</a></li>
         </li>
@@ -38,13 +36,10 @@
       <ul class="nav navbar-nav navbar-right">
         <li><a href="#">Link</a></li>
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Logout <span class="caret"></span></a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Se Connecter <span class="caret"></span></a>
           <ul class="dropdown-menu span2" role="menu">
-            <li><a href="profil">Profil</a></li>
             <li><a href="login">Login</a></li>
             <li><a href="admin">Administration</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Déconnexion</a></li>
           </ul>
         </li>
       </ul>
